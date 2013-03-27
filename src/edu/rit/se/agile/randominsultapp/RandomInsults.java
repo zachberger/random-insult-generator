6ea325f515f5f7bbb34c91eeea9602df0a32ebf1
Zach Berger <zsb1244@rit.edu>
2013-03-27 15:44:04 -0400

    Added basic on touch listener for the generate button

--- src/edu/rit/se/agile/randominsultapp/RandomInsults.java
+++ src/edu/rit/se/agile/randominsultapp/RandomInsults.java
@@ -3,13 +3,32 @@ package edu.rit.se.agile.randominsultapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class RandomInsults extends Activity {
 
+	private Button generateButton;
+	private TextView insultTextField;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_random_insults);
+		
+		insultTextField = (TextView) findViewById( R.id.insult_display );
+		generateButton = (Button) findViewById(R.id.button_generate);
+
+		generateButton.setOnClickListener( new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				insultTextField.setText("Some insult.");
+			}
+			
+		});
 	}
 
 	@Override
