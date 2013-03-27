caa9529fee152651cac8c2b3aed16430c583b71d
Zach Berger <zsb1244@rit.edu>
2013-03-27 15:34:33 -0400

    Added empty activity for viewing random insults

---
+++ src/edu/rit/se/agile/randominsultapp/RandomInsults.java
@@ -0,0 +1,22 @@
+package edu.rit.se.agile.randominsultapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class RandomInsults extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_random_insults);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.random_insults, menu);
+		return true;
+	}
+
+}
