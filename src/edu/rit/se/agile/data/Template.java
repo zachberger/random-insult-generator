48cf634829f73e70c79c859ca69809470e22cf2a
Mike Lentini <chvyvele@gmail.com>
2013-04-01 15:08:48 -0400

    Fixed some templetes.

--- src/edu/rit/se/agile/data/Template.java
+++ src/edu/rit/se/agile/data/Template.java
@@ -11,11 +11,11 @@ public class Template {
 	public void setId(long id) {
 		this.id = id;
 	}
-	public String getTemplete() {
+	public String getTemplate() {
 		return template;
 	}
-	public void setTemplete(String templete) {
-		this.template = templete;
+	public void setTemplate(String template) {
+		this.template = template;
 	}
 	
 	
