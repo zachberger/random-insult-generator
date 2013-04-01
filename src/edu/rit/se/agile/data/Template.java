fdd34d4e36a93e20fe70f712bfcf0265581320b4
Ian Graves <img4583@rit.edu>
2013-04-01 15:30:19 -0400

    Added Enum WordType for referencing types of words. Added word counting methods to Template

--- src/edu/rit/se/agile/data/Template.java
+++ src/edu/rit/se/agile/data/Template.java
@@ -1,5 +1,8 @@
 package edu.rit.se.agile.data;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Template {
 	
 	private long id;
@@ -8,16 +11,68 @@ public class Template {
 	public long getId() {
 		return id;
 	}
+	
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getTemplate() {
 		return template;
 	}
-	public void setTemplate(String template) {
+	
+	public void setTemplate(String templete) {
 		this.template = template;
 	}
 	
-	
+	/**
+	 * Returns how many adjective placeholders are in the template.
+	 */
+	public int adjCount() {
+		Pattern p = Pattern.compile(WordType.ADJECTIVE.val());
+	    Matcher m = p.matcher(template);
+	    int count = 0;
+	    while (m.find()){
+	    	count +=1;
+	    }
+		return count;
+	}
 
+	/**
+	 * Returns how many adverb placeholders are in the template.
+	 */
+	public int advCount() {
+		Pattern p = Pattern.compile(WordType.ADVERB.val());
+	    Matcher m = p.matcher(template);
+	    int count = 0;
+	    while (m.find()){
+	    	count +=1;
+	    }
+		return count;
+	}
+
+	/**
+	 * Returns how many noun placeholders are in the template.
+	 */
+	public int nounCount() {
+		Pattern p = Pattern.compile(WordType.NOUN.val());
+	    Matcher m = p.matcher(template);
+	    int count = 0;
+	    while (m.find()){
+	    	count +=1;
+	    }
+		return count;
+	}
+
+	/**
+	 * Returns how many verb placeholders are in the template.
+	 */
+	public int verbCount() {
+		Pattern p = Pattern.compile(WordType.VERB.val());
+	    Matcher m = p.matcher(template);
+	    int count = 0;
+	    while (m.find()){
+	    	count +=1;
+	    }
+		return count;
+	}
 }
