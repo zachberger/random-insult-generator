a37dd8519853e05a0b83035f41ba2df9a24655d0
Mike Lentini <chvyvele@gmail.com>
2013-04-03 15:38:27 -0400

    Add word UI is done.

--- src/edu/rit/se/agile/randominsultapp/AddWord.java
+++ src/edu/rit/se/agile/randominsultapp/AddWord.java
@@ -1,13 +1,30 @@
 package edu.rit.se.agile.randominsultapp;
 
+import java.util.List;
+
+import edu.rit.se.agile.data.Template;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class AddWord extends GenericActivity {
-
+	private Button addWordButton;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_word);
+		
+		addWordButton = (Button) findViewById(R.id.add_word_button);
+		
+		addWordButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(v.getContext(), "Do a thing.", Toast.LENGTH_SHORT).show();
+			}
+		});
 	}
 
 }
