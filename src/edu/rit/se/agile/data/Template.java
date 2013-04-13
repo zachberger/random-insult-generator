8a310a4be7ee6184c5fb9d6706d86a74d39a9726
cjl1750 <cjl1750@rit.edu>
2013-04-13 18:09:28 -0400

    Removed unused lines

--- src/edu/rit/se/agile/data/Template.java
+++ src/edu/rit/se/agile/data/Template.java
@@ -7,15 +7,15 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Template {
-	
+
 	private long id;
 	private String template;
 	private String category;
-	
+
 	public long getId() {
 		return id;
 	}
-	
+
 	public void setId(long id) {
 		this.id = id;
 	}
@@ -23,7 +23,7 @@ public class Template {
 	public String getTemplate() {
 		return template;
 	}
-	
+
 	public void setTemplate(String template) {
 		this.template = template;
 	}
@@ -33,28 +33,21 @@ public class Template {
 	public void setCategory(String category) {
 		this.category = category;
 	}
-	
+
 	public String fillTemplate(WordDAO wordDAO) {
 		String insult = template;
-		
-		Random rand = new Random();
-		
-		Pattern adjPattern  = Pattern.compile(WordType.ADJECTIVE.val());
-		Pattern advPattern  = Pattern.compile(WordType.ADVERB.val());
-		Pattern nounPattern = Pattern.compile(WordType.NOUN.val());
-		Pattern verbPattern = Pattern.compile(WordType.VERB.val());
-		
+
 		List<Word> adjWords = wordDAO.getAllWords(WordType.ADJECTIVE.val());
 		List<Word> advWords = wordDAO.getAllWords(WordType.ADVERB.val());
 		List<Word> nounWords = wordDAO.getAllWords(WordType.NOUN.val());
 		List<Word> verbWords = wordDAO.getAllWords(WordType.VERB.val());
-		
+
 		Collections.shuffle(adjWords);
 		Collections.shuffle(advWords);
 		Collections.shuffle(nounWords);
 		Collections.shuffle(verbWords);
-		
-		
+
+
 		String[] insultSplit = insult.split("\\s+");
 		String toReturn = "";
 		int count = 0;
@@ -68,25 +61,25 @@ public class Template {
 			} else if (insultSplit[count].contains(WordType.VERB.val())) {
 				insultSplit[count] = insultSplit[count].replace(WordType.VERB.val(), verbWords.get(0).getWord());
 			} 
-			toReturn +=  " " + insultSplit[count]; 
+
+			toReturn +=  " " + insultSplit[count];
 			count++;
 		}
-		
-		
-		
+
+
 		return toReturn;
-		
+
 	}
 	/**
 	 * Returns how many adjective placeholders are in the template.
 	 */
 	public int adjCount() {
 		Pattern p = Pattern.compile(WordType.ADJECTIVE.val());
-	    Matcher m = p.matcher(template);
-	    int count = 0;
-	    while (m.find()){
-	    	count +=1;
-	    }
+		Matcher m = p.matcher(template);
+		int count = 0;
+		while (m.find()){
+			count +=1;
+		}
 		return count;
 	}
 
@@ -95,11 +88,11 @@ public class Template {
 	 */
 	public int advCount() {
 		Pattern p = Pattern.compile(WordType.ADVERB.val());
-	    Matcher m = p.matcher(template);
-	    int count = 0;
-	    while (m.find()){
-	    	count +=1;
-	    }
+		Matcher m = p.matcher(template);
+		int count = 0;
+		while (m.find()){
+			count +=1;
+		}
 		return count;
 	}
 
@@ -108,11 +101,11 @@ public class Template {
 	 */
 	public int nounCount() {
 		Pattern p = Pattern.compile(WordType.NOUN.val());
-	    Matcher m = p.matcher(template);
-	    int count = 0;
-	    while (m.find()){
-	    	count +=1;
-	    }
+		Matcher m = p.matcher(template);
+		int count = 0;
+		while (m.find()){
+			count +=1;
+		}
 		return count;
 	}
 
@@ -121,11 +114,11 @@ public class Template {
 	 */
 	public int verbCount() {
 		Pattern p = Pattern.compile(WordType.VERB.val());
-	    Matcher m = p.matcher(template);
-	    int count = 0;
-	    while (m.find()){
-	    	count +=1;
-	    }
+		Matcher m = p.matcher(template);
+		int count = 0;
+		while (m.find()){
+			count +=1;
+		}
 		return count;
 	}
 
@@ -134,5 +127,5 @@ public class Template {
 		return "Template [id=" + id + ", template=" + template + ", category="
 				+ category + "]";
 	}
-	
+
 }
