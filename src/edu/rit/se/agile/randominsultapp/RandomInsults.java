0038dce3ee52f5265b1917dba542d210194f5c56
cjl1750 <cjl1750@rit.edu>
2013-04-17 14:11:18 -0400

    Removed imports and other warnings.

--- src/edu/rit/se/agile/randominsultapp/RandomInsults.java
+++ src/edu/rit/se/agile/randominsultapp/RandomInsults.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Random;
 
 import android.database.Cursor;
-import android.database.sqlite.SQLiteCursor;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
